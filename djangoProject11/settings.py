a82fb9f0a96251373077cbdecdfc890ca0848ecc
Harshit Pandey <harshitpandey902@gmail.com>
2021-08-23 16:07:55 +0530

    .

--- djangoProject11/settings.py
+++ djangoProject11/settings.py
@@ -21,13 +21,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r-m=4acsw&6l$+d*1=wh69(4-7k6j-_#1o8@dh%xjdabzmp6&u'
+#SECRET_KEY = 'django-insecure-r-m=4acsw&6l$+d*1=wh69(4-7k6j-_#1o8@dh%xjdabzmp6&u'
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure -r-m=4acsw&6l$+d*1=wh69(4-7k6j-_#1o8@dh%xjdabzmp6&u")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+SECURE_HSTS_SECONDS = 31536000
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+CSRF_COOKIE_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_PRELOAD = True
 
 # Application definition
 
